a020c8e9f08f36e71fd819a0fd77770fe1658220
Santiago San Román <santi72alc@gmail.com>
2020-01-24 20:12:37 +0100

    Se deja listo para el visionado y borrado de un artículo

--- routes/index.js
+++ routes/index.js
@@ -16,24 +16,64 @@ router.get('/', function (req, res, next) {
 
 
 /**
- * Para versiones futuras de DEMO WEB
+ * Para versiones de DEMO WEB
  */
+// Para la ventanta principal del CRUD
 router.get('/crud', async function (req, res, next) {
+  console.log(req.body);
   const consult = await Articles.list({});
   res.render('crudArticles', { result: consult });
 });
 
+
+// Para añadir el registro
 router.post('/crud/add', async function (req, res, next) {
   ctrls.add(req, res, next);
   res.redirect('/crud');
 });
 
-router.delete('crud/delete/:id', async function(req, res, next) {
-  const id =  req.params.id;
-  await Articles.remove( { _id: id });
+// Para borrar el registro
+router.get('/crud/delete/:id', async function(req, res, next) {
+  const { id } =  req.params;
+  await Articles.deleteOne({ _id: id }).exec();
   res.redirect('/crud');
 })
 
+
+// Para ver el registro
+router.get('/crud/view/:id', async function(req, res, next) {
+  const { id } =  req.params;
+  const consult = await Articles.list({ filter:{_id: id} });
+  res.render('viewArticle', { result: consult } );
+})
+
+
+
+// Para el filtro
+router.get('/crud/view', async function (req, res, next) {
+  let limit = (req.body.limit) ? req.body.limit : null;
+  let skip = (req.body.skip) ? req.body.skip : null;
+  let fields = (req.body.fields) ? req.body.fields : null;
+  let sort = (req.body.sort) ? req.body.sort : null;
+  let filter = (req.body.filter) ? { "filter": req.body.name + req.body.txtName } : null;
+  // let limit;
+  // let skip;
+  // let fields;
+  // let sort;
+  console.log(req.body);
+  const consult = await Articles.list({ filter, limit, skip, fields, sort });
+  res.redirect('/crud');
+});
+
+
+// Para la visata de uno
+router.get('/crud/view/:id', async function (req, res, next) {
+  let id = req.params.id;
+
+  console.log(req.params);
+  const consult = await Articles.list({ _id: id} );
+  res.redirect('/crud');
+});
 
 
 
