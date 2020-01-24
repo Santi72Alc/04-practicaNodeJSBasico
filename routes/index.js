1dc8ef395053164d9a53e7443bac968d4e68faa7
Santiago San Román <santi72alc@gmail.com>
2020-01-24 20:12:37 +0100

    creando acceso WEB

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,32 @@ const ctrls = require('../controllers/articles');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  res.render('indexWEB');
 });
 
 
 /**
  * Para versiones futuras de DEMO WEB
  */
-// router.get('/crud', async function ( req, res, next) {
-//   const consult = await Articles.list({});
-//   console.log("Consulta: ", consult);
-//   res.render('crudArticles', { result: consult });
-// });
+router.get('/crud', async function (req, res, next) {
+  const consult = await Articles.list({});
+  res.render('crudArticles', { result: consult });
+});
+
+router.post('/crud/add', async function (req, res, next) {
+  ctrls.add(req, res, next);
+  res.redirect('/crud');
+});
+
+router.delete('crud/delete/:id', async function(req, res, next) {
+  const id =  req.params.id;
+  await Articles.remove( { _id: id });
+  res.redirect('/crud');
+})
+
+
+
 
 // router.get('/crud/add', async function ( req, res, next) {
 //   const body = req.body;
